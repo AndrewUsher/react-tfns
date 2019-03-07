3c99978b30edbf90293b84a83515f55817edd366
Andrew Usher <andrewusher00@gmail.com>
2019-03-07 07:19:02 -0600

    add package name to rollup config for build

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,20 @@
 import typescript from 'rollup-plugin-typescript2'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+
+const packageName = pkg.name.replace('@drewster', '')
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/bundle.esm.js',
+      file: `dist/${packageName}.esm.js`,
       format: 'esm'
+    },
+    {
+      file: `dist/${packageName}.cjs.js`,
+      format: 'cjs'
     }
   ],
   plugins: [

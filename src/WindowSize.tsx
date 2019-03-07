0e0d00bb396b67d624342881b00ddbc5a50298fb
Andrew Usher <andrewusher00@gmail.com>
2019-03-06 20:50:40 -0600

    Add WindowSize component

---
+++ src/WindowSize.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { debounce } from 'debounce'
+
+function useWindowSize() {
+  const [state, setState] = useState({
+    height: 0,
+    width: 0
+  })
+
+  const handleResize = () =>
+    setState({
+      height: window.innerHeight,
+      width: window.innerWidth
+    })
+
+  useEffect(() => {
+    setState({
+      height: window.innerHeight,
+      width: window.innerWidth
+    })
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return state
+}
+
+const WindowSize = ({ children, render }) => {
+  const { height, width } = useWindowSize()
+  if (render) {
+    return render({ height, width })
+  } else if (children) {
+    return children({ height, width })
+  } else {
+    return null
+  }
+}
+
+export default WindowSize
